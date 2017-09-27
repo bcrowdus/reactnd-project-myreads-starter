45ed75a1aeaad2015c8842754d2b978057ae8d44
Brinn Crowdus <brinn@crowdus.com>
2017-09-27 01:04:55 -0400

    refactor : Refactor BooShelfChanger component to stateless component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,28 +1,23 @@
 import React from 'react'
 import BooksGrid from "./BooksGrid";
 
-class BookShelf extends React.Component {
-  	onNoBooks = () => {
-        const { title } = this.props;
+const BookShelf = ({ title, books, onBookShelfChange }) => {
+    const onNoBooks = () => {
+        return (
+			<h2>The {title} bookshelf is empty.</h2>
+        )
+    };
 
-  		return (
-  			<h2>The {title} bookshelf is empty.</h2>
-		)
-	}
-	render() {
-		const { title, books, onBookShelfChange } = this.props;
-
-		return (
-			<div className="bookshelf">
-				<h2 className="bookshelf-title">{ title }</h2>
-				<div className="bookshelf-books">
-					<BooksGrid books={books} 
-          				onBookShelfChange={onBookShelfChange} 
-          				onNoBooks={this.onNoBooks}/>
-				</div>
+	return (
+		<div className="bookshelf">
+			<h2 className="bookshelf-title">{ title }</h2>
+			<div className="bookshelf-books">
+				<BooksGrid books={books}
+						   onBookShelfChange={onBookShelfChange}
+						   onNoBooks={onNoBooks}/>
 			</div>
-		)
-	}
-}
+		</div>
+	);
+};
 
 export default BookShelf
