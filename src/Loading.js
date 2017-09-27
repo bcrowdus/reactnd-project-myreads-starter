aec96705af72c568e4a3b42d88b81b456e939d4a
Brinn Crowdus <brinn@crowdus.com>
2017-09-27 00:43:09 -0400

    refactor : Refactor Loading component to stateless component

--- src/Loading.js
+++ src/Loading.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
-class Loading extends React.Component {
-	render() {
-      	const { visible } = this.props;      
-      	const className = visible ? "search-loading" : "search-loading hidden";
-        
-		return (<div className={className}></div> );	
-    }
-}
+const Loading = ({ visible }) => {
+	const className = visible ? "search-loading" : "search-loading hidden";
+    return ( <div className={className}></div> );
+};
                 
 export default Loading;
