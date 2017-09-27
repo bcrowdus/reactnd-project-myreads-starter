d8db81e72c5d80e214aa932690391783d2ac446f
Brinn Crowdus <brinn@crowdus.com>
2017-09-27 00:24:10 -0400

    refactor : Refactor BookCase component to stateless component

--- src/BookCase.js
+++ src/BookCase.js
@@ -1,34 +1,28 @@
 import React from 'react'
 import BookShelf from './BookShelf'
 
-class BooksCase extends React.Component {
-  	
-	bookFilter = (filter) => (book) => book.shelf === filter;
-
-	currentlyReadingFilter = this.bookFilter('currentlyReading');
-    wantToReadFilter = this.bookFilter('wantToRead');
-    readFilter = this.bookFilter('read');
+const BookCase = ({ books, onBookShelfChange}) => {
+	const bookFilter = (filter) => (book) => book.shelf === filter;
+	const currentlyReadingFilter = bookFilter('currentlyReading');
+    const wantToReadFilter = bookFilter('wantToRead');
+    const readFilter = bookFilter('read');
 	
-	render() {
-		const { books, onBookShelfChange } = this.props
-		
-		return (
-			<div className="list-books-content">
-      			<div>
-					<BookShelf title="Currently Reading"
-							   books={books.filter( this.currentlyReadingFilter )}
-							   onBookShelfChange={onBookShelfChange} />
+	return (
+		<div className="list-books-content">
+			<div>
+				<BookShelf title="Currently Reading"
+						   books={books.filter( currentlyReadingFilter )}
+						   onBookShelfChange={onBookShelfChange} />
 
-					<BookShelf title="Want to Read"
-							   books={books.filter( this.wantToReadFilter  ) }
-							   onBookShelfChange={onBookShelfChange} />
+				<BookShelf title="Want to Read"
+						   books={books.filter( wantToReadFilter  ) }
+						   onBookShelfChange={onBookShelfChange} />
 
-					<BookShelf title="Read"
-							   books={books.filter( this.readFilter )}
-							   onBookShelfChange={onBookShelfChange} />
-				</div>
+				<BookShelf title="Read"
+						   books={books.filter( readFilter )}
+						   onBookShelfChange={onBookShelfChange} />
 			</div>
-		)
-	}
-}
-export default BooksCase
+		</div>
+	)
+};
+export default BookCase;
